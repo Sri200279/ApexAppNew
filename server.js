7c21185499061d358c670d2bf72bdb3f5b0e650e
Sri200279 <164613160+Sri200279@users.noreply.github.com>
2025-08-20 09:43:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -80,9 +80,7 @@ app.post("/approve-payment",  async(req, res) => {
 
     // Fetch payment for email
     const paymentRes = await pool.query("SELECT * FROM payments WHERE id = $1", [id]);
-   if (!paymentRes) {
-      return res.status(404).json({ error: "Payment not found" });
-    }
+   
     const payment = paymentRes.rows[0];
     if (!payment) {
       return res.status(404).json({ error: "Payment not found" });
@@ -161,6 +159,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,"0.0.0.0",() => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
 
 
 
