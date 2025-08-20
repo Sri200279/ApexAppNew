3e0464ad651aaf993c42b02a5d89a5b57e03a58c
Sri200279 <164613160+Sri200279@users.noreply.github.com>
2025-08-20 09:17:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -159,9 +159,10 @@ app.get("/init", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
 
 
 
