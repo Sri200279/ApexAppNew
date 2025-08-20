6b0c15610b77f4233db967bbfb409ca8f7c39efc
Sri200279 <164613160+Sri200279@users.noreply.github.com>
2025-08-21 01:11:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get("/list-approved", async (req, res) => {
 
 app.post("/approve-ans", async (req, res) => {
   try {
-    const {id}  = req.body.id; // ✅ directly get id from query
+    const {id,question,answer}  = req.body; // ✅ directly get id from query
     if (!id) {
       return res.status(400).json({ error: "questionId is required" });
     }
@@ -102,7 +102,7 @@ app.post("/approve-ans", async (req, res) => {
     // Save Q&A
     await pool.query(
       "INSERT INTO GD (question, answer) VALUES ($1, $2)",
-      [question1, answer1]
+      [question, answer]
     );
 
     // Fetch payment for email
@@ -235,6 +235,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,"0.0.0.0",() => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
 
 
 
