b12dcad8d40603b127c69449314435f0311f6485
Sri200279 <164613160+Sri200279@users.noreply.github.com>
2025-08-20 09:07:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config();
+
 const pool = new Pool({
   connectionString: process.env.DB_KEY,
   ssl: { rejectUnauthorized: false }
@@ -159,6 +161,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
 
 
 
