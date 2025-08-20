b503c18dc4df128be9977999c8340c085de3c0a1
Sri200279 <164613160+Sri200279@users.noreply.github.com>
2025-08-20 08:18:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 const app = express();
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json);
@@ -156,6 +156,7 @@ app.get("/init", async (req, res) => {
 
 
 app.listen(5000, () => console.log("✅ Server running on http://localhost:5000"));
+
 
 
 
