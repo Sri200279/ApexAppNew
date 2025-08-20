e6fb3286afde0c5a02c90c8fff71a3c241f3542c
Sri200279 <164613160+Sri200279@users.noreply.github.com>
2025-08-21 00:34:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -93,6 +93,7 @@ app.get("/list-approved", async (req, res) => {
 app.post("/approve-ans", async (req, res) => {
   try {
     const { id } = req.query; // ✅ directly get id from query
+    console.log(id);
     if (!id) {
       return res.status(400).json({ error: "questionId is required" });
     }
@@ -234,6 +235,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,"0.0.0.0",() => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
 
 
 
